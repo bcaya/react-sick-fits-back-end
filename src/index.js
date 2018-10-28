66dc3ccf65b12fab706d712a2009ee1985ae08bd
Bobby Caya <bob.caya1@gmail.com>
2018-10-28 17:32:11 -0600

    function for create server

--- src/index.js
+++ src/index.js
@@ -6,3 +6,12 @@ const server = createServer();
 
 // Need to complete: User express middleware to handle cookies(JWT)
 // Need to complete: use express middleware to populate current user
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  },
+}, deets => {
+  console.log(`Server is now running on port http:/localhost:${deets.port}`)
+})
