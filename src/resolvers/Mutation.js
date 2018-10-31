c9dca201f848025755fcadeb4f2787e9889e38fd
Bobby Caya <bob.caya1@gmail.com>
2018-10-30 19:28:19 -0600

    add model for item

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,10 @@
-const Mutations = {};
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    //create a dog
+    const newDog = {name: args.name };
+    console.log(args);
+  }
+};
 
 module.exports = Mutations;
