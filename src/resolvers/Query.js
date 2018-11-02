159846644328439a8d1967ab86a04a59254c6220
Bobby Caya <bob.caya1@gmail.com>
2018-11-02 16:17:30 -0600

    update to schema for update items

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,6 +1,12 @@
-const Query = {
-  // dogs(parent, args, ctx, info ){
-  //   return [{name: 'Nyx'}, {name: 'Ruka'}]
+ const { forwardTo } = require('prisma-binding');
+ 
+ const Query = {
+   items: forwardTo('db'),
+   item: forwardTo('db'),
+  // async items(parent, args, ctx, info) {
+  //   console.log('Getting items!')
+  //   const items = await ctx.db.query.items();
+  //   return items;
   // }
 };
 
