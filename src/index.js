0ff93a79956e46672c78e529f21a3cda5cab989f
Bobby Caya <bob.caya1@gmail.com>
2018-11-11 19:57:49 -0700

    add cookie parser to have authentication via cookie

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+const cookieParser = require('cookie-parser');
+
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-// Need to complete: User express middleware to handle cookies(JWT)
+server.express.use(cookieParser());
 // Need to complete: use express middleware to populate current user
 
 server.start({
