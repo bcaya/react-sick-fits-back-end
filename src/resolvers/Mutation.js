085c2f74c4dd3565a9984ea2aea8ca113851e5a9
Bobby Caya <bob.caya1@gmail.com>
2018-12-03 19:55:00 -0700

    mutation js

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -106,7 +106,44 @@ const Mutations = {
     return {message: 'Thanks!'}
     //3. email them that reset token
 
-  }
+  },
+  async resetPassword(parent, args, ctx, info){
+    //1. check if the passwords match
+    if(args.password !== args.confirmPassword) {
+      throw new Error('Sorry the Passwords don\t match!')
+    }
+    //2. check if it is a legit reset token
+    //3. check if its expired
+    const [user] = await ctx.db.query.users({
+      where: {
+        resetToken: args.resetToken,
+        resetTokenExpiry_gte: Date.now() - 3600000
+      }
+    });
+    if (!user){
+      throw new Error('This token is either invalid or expired!');
+    }
+    //4. hash their new password
+    const password = await bcrypt.hash(args.password, 10);
+    //5. save the new password to the user and remove old resetToken fields
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: {email: user.email },
+      data: {
+        password,
+        resetToken: null,
+        resetTokenExpiry: null,
+      },
+    });
+    //6. generate JWT
+    const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+    //7 set the JWT cooke
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+    //8. return the new user
+    return updatedUser;
+  },
 };
 
 module.exports = Mutations;
