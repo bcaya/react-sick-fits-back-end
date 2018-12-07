9ac22db77781ea2fd12322ab23c99e6ad62190e3
Bobby Caya <bob.caya1@gmail.com>
2018-12-06 20:52:44 -0700

    set up fake mailer

---
+++ src/mail.js
@@ -0,0 +1,26 @@
+const nodemail = require('nodemailer');
+const transport = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
+  },
+});
+
+const makeAniceEmail = text => `
+  <div className="email" style="
+    border: 1px solid black;
+    padding: 20px;
+    font-family: sans-serif;
+    line-height: 2;
+    font-size: 20px;
+  ">
+    <h2>Howdy There!</h2>
+    <p>${text}</p>
+    <p>🤠, Bobby C. </p>
+  </div>
+`;
+
+exports.transport = transport;
+exports.makeAniceEmail = makeAniceEmail;
